759e0a8208569f968fbd8fef3d0d462c9d8a5c81
Rochelle Awuor <awuorochelle75@gmail.com>
2025-06-01 16:59:05 +0300

    modifies files

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id_to_find):
+        def dfs(node):
+            if node is None:
+                return None
+            if node.get('id') == id_to_find:
+                return node
+            for child in node.get('children', []):
+                found = dfs(child)
+                if found:
+                    return found
+            return None
+
+        return dfs(self.root)
